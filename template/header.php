cedb1e49e1a909102b4bc11fbe6190c12dbaccd6
compmaxxx <compmaxxx@gmail.com>
2014-12-13 17:45:10 +0700

    header tab

--- template/header.php
+++ template/header.php
@@ -1,5 +1,5 @@
 <?php
-  $pages = array("frequency"=>"Frequency");
+  $pages = array("frequency"=>"Frequency","sunami"=>"Tsunami");
   $active = array();
   foreach($pages as $name=>$short)
   {
@@ -14,16 +14,31 @@
     <script src="asset/jquery-1.11.1.min.js"></script>
     <script src="asset/bootstrap/js/bootstrap.min.js"></script>
     <script src="asset/d3/d3.min.js"></script>
+    <style>
+      .nav-justified{
+        background-color:#2c3e50;
+        text-transform:uppercase;
+        text-align:justify;
+        border-radius:5;
+        overflow:hidden;
+      }
+      .nav-justified > li > a{
+        color:white;
+      }
+    </style>
+
+
   </head>
   <body>
     <div class="container">
+      <div class="page-header">
+        <h1>ProjectName</h1>
+      </div>
       <ul class="nav nav-tabs nav-justified">
-        <h3>EarthQuake</h3>
-      </ul>
-      <ul class="nav nav-pills nav-justified">
         <? foreach($pages as $name=>$short){  ?>
-          <li role="presentation" <? if($active[$name]) { ?>  class="active"  <? } ?>>
+          <li <? if($active[$name]) { ?>  class="active"  <? } ?>>
             <a href="<?=$name?>.php"><?=$short?></a></li>
             <? } ?>
       </ul>
+      </div>
     </div>
