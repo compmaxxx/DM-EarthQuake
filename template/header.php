0cbe6d3e79a27a2fb650aff310b48deaef14ac1f
compmaxxx <compmaxxx@gmail.com>
2014-12-13 18:16:47 +0700

    add home tab

--- template/header.php
+++ template/header.php
@@ -1,5 +1,5 @@
 <?php
-  $pages = array("frequency"=>"Frequency","sunami"=>"Tsunami");
+  $pages = array("index"=>"home","frequency"=>"Frequency","sunami"=>"Tsunami");
   $active = array();
   foreach($pages as $name=>$short)
   {
