5d848adeae132528e19ad51a0e3772b2ceaa06e9
compmaxxx <compmaxxx@gmail.com>
2014-12-13 15:16:06 +0700

    initial asset

---
+++ header.inc.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <meta charset="utf8">
+    <title>EarthQuake</title>
+    <link rel="stylesheet" href="asset/bootstrap/css/bootstrap.min.css"/>
+    <script src="asset/jquery-1.11.1.min.js"></script>
+    <script src="asset/bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body>
