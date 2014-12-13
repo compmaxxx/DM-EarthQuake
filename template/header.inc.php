230e49f482cc6abb889a0f65b200648bc27fd7ec
compmaxxx <compmaxxx@gmail.com>
2014-12-13 15:36:48 +0700

    add d3 local

--- template/header.inc.php
+++ template/header.inc.php
@@ -5,6 +5,6 @@
     <link rel="stylesheet" href="asset/bootstrap/css/bootstrap.min.css"/>
     <script src="asset/jquery-1.11.1.min.js"></script>
     <script src="asset/bootstrap/js/bootstrap.min.js"></script>
-    <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+    <script src="asset/d3/d3.min.js"></script>
   </head>
   <body>
