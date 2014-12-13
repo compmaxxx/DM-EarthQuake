33446f4ee2855c4eb14028b81e1762016cb2fb31
panisone <cooky.ky@hotmail.com>
2014-12-13 19:00:19 +0700

    sunami

--- action_sunami.php
+++ action_sunami.php
@@ -1,54 +1,99 @@
 <?php include_once "template/header.php" ?>
 
 <?php
+	$input_ctn = $_POST["continent"];
 	$input_mag = $_POST["magnitude"];
 	$input_lat = $_POST["latitude"];
 	$input_lng = $_POST["longtitude"];
 
-	$mag = array(0.123,0,0.139);
-	$lat = array(0.029,0,0.016);
-	$lng = array(-0.042,0,0);
-	$oth = array(-0.047,0,-0.045);
+	//array: all, africa, antarctica, asia, europe, north, oceanic, south
+	$mag = array(0.123,0,0.994,0.206,0,0.139,0.564,0);
+	$lat = array(0.029,0,0,0,0,0.016,0.200,0);
+	$lng = array(-0.042,0,0,0,0,0,-0.084,0);
+	$oth = array(-0.047,0,-0.174,-0.054,0,-0.045,-0.373,0);
 
 	$sum = 0;
+	$i = 0;
 	
-	$sd_mag = array(1.155,1,0.834);
-	$mean_mag = array(1.669,0,1.451);
+	$sd_mag = array(1.155,1,0.465,0.442,1,0.834,0.963,1);
+	$mean_mag = array(1.669,0,5.144,4.736,0,1.451,4.593,0);
 
-	$sd_lat = array(17.215,1,13.048);
-	$mean_lat = array(41.336,0,43.607);
+	$sd_lat = array(17.215,1,1.498,17.107,1,13.048,31.321,1);
+	$mean_lat = array(41.336,0,-56.263,16.330,0,43.607,0.352,0);
 
-	$sd_lng = array(58.913,1,33.404);
-	$mean_lng = array(-113.013,0,-123.705);
-	
+	$sd_lng = array(58.913,1,4.626,29.546,1,33.404,142.013,1);
+	$mean_lng = array(-113.013,0,-28.153,108.464,0,-123.705,0.353,0);
+
+	$max = array(0.081);
+	$min = array(-0.033)
 ?>
 
-TEST KiE
+Result: Linear-Regession
+
+<?php
+	if ($input_ctn == "all") {
+		$i = 0;
+	}
+	elseif ($input_ctn == "africa") {
+		$i = 1;
+	}
+	elseif ($input_ctn == "antarctica") {
+		$i = 2;
+	}
+	elseif ($input_ctn == "asia") {
+		$i = 3;
+	}
+	elseif ($input_ctn == "europe") {
+		$i = 4;
+	}
+	elseif ($input_ctn == "north") {
+		$i = 5;
+	}
+	elseif ($input_ctn == "oceanic") {
+		$i = 6;
+	}
+	elseif ($input_ctn == "south") {
+		$i = 7;
+	}
+?>
+
 <br>
+<?php echo "continent: $input_ctn"; ?>
+<br><br>
 <?php echo "mag: $input_mag"; ?>
 <br>
 <?php 
-	$nor_mag = ($input_mag - $mean_mag) / $sd_mag;
+	$nor_mag = ($input_mag - $mean_mag[$i]) / $sd_mag[$i];
 	echo "nor_mag: $nor_mag"; ?>
 <br><br>
 <?php echo "lat: $input_lat"; ?>
 <br>
 <?php 
-	$nor_lat = ($input_lat - $mean_lat) / $sd_lat;
+	$nor_lat = ($input_lat - $mean_lat[$i]) / $sd_lat[$i];
 	echo "nor_lat: $nor_lat"; ?>
-<br>
 <br><br>
 <?php echo "lng: $input_lng"; ?>
 <br>
 <?php 
-	$nor_lng = ($input_lng - $mean_lng) / $sd_lng;
+	$nor_lng = ($input_lng - $mean_lng[$i]) / $sd_lng[$i];
 	echo "nor_lng: $nor_lng"; ?>
-<br>
-
+<br><br>
+<?php
+	$sum = ($nor_mag * $mag[$i]) + ($nor_lat * $lat[$i]) + ($nor_lng * $lng[$i]) + $oth[$i];
+	echo "regession: $sum";
+?>
 <br>
 <?php
-	$sum = ($nor_mag * $mag[0]) + ($nor_lat * $lat) + ($nor_lng * $lng) + $oth;
-	echo "regession: $sum";
+	if ($sum < $min[$i]) {
+		$nor_sum = 0;
+	}
+	elseif ($sum > $max[$i]) {
+		$nor_sum = 1;
+	}
+	else {
+		$nor_sum = ($sum - $min[$i]) / ($max[$i] - $min[$i]);
+	}
+	echo "nor_regession: $nor_sum";
 ?>
 
 <?php include_once "template/footer.php" ?>
