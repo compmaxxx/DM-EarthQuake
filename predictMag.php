40bf3d224d01b02106b3c0fe57c84be02a33dcbe
Thitima <sokillt@gmail.com>
2014-12-18 14:19:48 +0700

    แก้ css

--- predictMag.php
+++ predictMag.php
@@ -1,13 +1,17 @@
 <?php include_once "template/header.php" ?>
-PREDICT MAGNITUDE BY REGRESSION
+
 <!--dmin = [0,44.507], gap = [10,356.4]-->
 
-<form action="magRegression.php" method="post">
-	dmin	: <input name="dmin" type="number" min="0" max="44.507" step="any"><br>
-	gap		: <input name="gap" type="number" min="10" max="356.4" step="any"><br>
-	lat		: <input name="lat" type="number" min="-90" max="90" step="any"><br>
-	lng		: <input name="lng" type="number" min="-180" max="180" step="any"><br>
-	<button type="submit">Enter</button>
+
+<form action="magRegression.php" method="post" class="form-inline" role="form">
+	<div class="form-group">
+		<input class="form-control" name="dmin" type="number" min="0" max="44.507" step="any" placeholder="Enter DMIN Value">
+		<input class="form-control" name="gap" type="number" min="10" max="356.4" step="any" placeholder="Enter GAP Value">
+		<input class="form-control" name="lat" type="number" min="-90" max="90" step="any" placeholder="Enter Latitude Value">	
+		<input class="form-control" name="lng" type="number" min="-180" max="180" step="any" placeholder="Enter Longitude Value">
+		
+	<button type="submit" class="btn btn-default">Enter</button>
+	</div>
 </form>
 
 
