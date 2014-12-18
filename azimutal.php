771351a25fc7f39889af51bf1b198b6e01d2ccec
compmaxxx <compmaxxx@gmail.com>
2014-12-18 15:42:42 +0700

    add azimuthal plain earth

---
+++ azimutal.php
@@ -0,0 +1,113 @@
+<?php include_once "template/header.php" ?>
+<script src="asset/d3/d3.geo.js"></script>
+<style>
+
+  svg {
+    width: 1280px;
+    height: 800px;
+    pointer-events: all;
+  }
+
+  circle {
+    fill: #dbe4f0;
+  }
+
+  path {
+    fill: #3e89ca;
+    stroke: #fff;
+  }
+
+</style>
+
+<div id="body">
+
+</div>
+
+
+<script>
+
+  var feature;
+
+  var projection = d3.geo.azimuthal()
+  .scale(380)
+  .origin([-71.03,42.37])
+  .mode("orthographic")
+  .translate([640, 400]);
+
+  var circle = d3.geo.greatCircle()
+  .origin(projection.origin());
+
+  // TODO fix d3.geo.azimuthal to be consistent with scale
+  var scale = {
+    orthographic: 380,
+    stereographic: 380,
+    gnomonic: 380,
+    equidistant: 380 / Math.PI * 2,
+    equalarea: 380 / Math.SQRT2
+  };
+
+  var path = d3.geo.path()
+  .projection(projection);
+
+  var svg = d3.select("#body").append("svg:svg")
+  .attr("width", 1280)
+  .attr("height", 800)
+  .on("mousedown", mousedown);
+
+
+  d3.json("asset/d3/world-countries.json", function(error, collection) {
+    feature = svg.selectAll("path")
+    .data(collection.features)
+    .enter().append("svg:path")
+    .attr("d", clip);
+
+    feature.append("svg:title")
+    .text(function(d) { return d.properties.name; });
+  });
+
+  d3.select(window)
+  .on("mousemove", mousemove)
+  .on("mouseup", mouseup);
+
+  d3.select("select").on("change", function() {
+    projection.mode(this.value).scale(scale[this.value]);
+    refresh(750);
+  });
+
+  var m0,
+  o0;
+
+  function mousedown() {
+    m0 = [d3.event.pageX, d3.event.pageY];
+    o0 = projection.origin();
+    d3.event.preventDefault();
+  }
+
+  function mousemove() {
+    if (m0) {
+      var m1 = [d3.event.pageX, d3.event.pageY],
+      o1 = [o0[0] + (m0[0] - m1[0]) / 8, o0[1] + (m1[1] - m0[1]) / 8];
+      projection.origin(o1);
+      circle.origin(o1)
+      refresh();
+    }
+  }
+
+  function mouseup() {
+    if (m0) {
+      mousemove();
+      m0 = null;
+    }
+  }
+
+  function refresh(duration) {
+    (duration ? feature.transition().duration(duration) : feature).attr("d", clip);
+  }
+
+  function clip(d) {
+    return path(circle.clip(d));
+  }
+
+
+</script>
+<?php include_once "template/footer.php" ?>
